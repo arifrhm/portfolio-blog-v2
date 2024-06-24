6e4449cf5a8311e9a9d371ebda38b39be6803b8d
Arif Rahman <arif.rahman.rhm@gmail.com>
2024-06-24 22:38:15 +0700

    fix (Header): line-through effect in corresponding page

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,8 +1,12 @@
+"use client";
+
 import React from "react";
-// import Search from "./Search";
+import { usePathname } from 'next/navigation';
 import SearchBar from "./SearchBar";
 
 const Header: React.FC = () => {
+  const currentPath = usePathname();
+
   return (
     <header className="border-b border-gray-200 py-4">
       <div className="container mx-auto flex justify-between items-center px-4">
@@ -12,11 +16,11 @@ const Header: React.FC = () => {
           </a>
         </div>
         <nav className="flex space-x-6">
-          <a href="/blog" className="text-gray-600 hover:line-through">blog</a>
-          <a href="/portfolio" className="text-gray-600 hover:line-through">portfolio</a>
-          <a href="/photo" className="text-gray-600 hover:line-through">photo</a>
-          <a href="/about" className="text-gray-600 hover:line-through">about</a>
-          <a href="/contact" className="text-gray-600 hover:line-through">contact</a>
+          <a href="/blog" className={`text-gray-600 ${currentPath === '/blog' ? 'line-through' : 'hover:line-through'}`}>blog</a>
+          <a href="/portfolio" className={`text-gray-600 ${currentPath === '/portfolio' ? 'line-through' : 'hover:line-through'}`}>portfolio</a>
+          <a href="/photo" className={`text-gray-600 ${currentPath === '/photo' ? 'line-through' : 'hover:line-through'}`}>photo</a>
+          <a href="/about" className={`text-gray-600 ${currentPath === '/about' ? 'line-through' : 'hover:line-through'}`}>about</a>
+          <a href="/contact" className={`text-gray-600 ${currentPath === '/contact' ? 'line-through' : 'hover:line-through'}`}>contact</a>
         </nav>
         <div className="text-gray-600">
           <SearchBar />
